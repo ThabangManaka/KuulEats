d9c89fd85d41b435d6cf8e0fc255337ffabc76d0
ThabangManaka <thabangdansteyn@gmail.com>
2022-08-05 07:46:51 +0200

    Added  Controller For Order

--- KuulEats/Controllers/OrderController.cs
+++ KuulEats/Controllers/OrderController.cs
@@ -1,10 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using KuulEats.Interfaces;
+using KuulEats.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KuulEats.Controllers;
+[Route("api/[controller]")]
 public class OrderController : Controller
 {
-    public IActionResult Index()
+
+    private readonly IUnitOfWork uow;
+    private readonly IMapper mapper;
+
+    public OrderController(IUnitOfWork uow, IMapper mapper)
+        {
+            this.uow = uow;
+            this.mapper = mapper;
+        }
+
+
+    [HttpGet]
+    public IEnumerable<Order> Get()
     {
-        return View();
+        return uow.OrderRepository.GetAllOrder();
     }
+
+    [HttpPost("add")]
+    public async Task<IActionResult> Post([FromBody] Order orders)
+    {
+        uow.OrderRepository.InsertOrder(orders);
+        await uow.SaveAsync();
+        return StatusCode(201);
+    }
+
+
+
 }
